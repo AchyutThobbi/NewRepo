c29cbfc66545be3bd924de71fe02354f12a922f5
AchyutThobbi <89890314+AchyutThobbi@users.noreply.github.com>
2021-09-03 08:53:48 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ class Hello
 {
 	public static void main(String args[])
 	{
-		system.out.println("Hello, this is from feature branch")
+		system.out.println("Hello, this is from main branch")
 	}
 }
